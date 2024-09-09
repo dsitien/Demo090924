9cb5c50c4dca2bba64d5e7f55e64b9b199eba623
Si Tien <duongsitien123@gmail.com>
2024-09-09 11:12:43 +0700

    finish demo

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using WebAPIDemo.DataAccess;
 using WebAPIDemo.Model;
+using WebAPIDemo.Model.Entity;
 
 namespace WebAPIDemo.Controllers
 {
@@ -21,5 +22,43 @@ namespace WebAPIDemo.Controllers
         {
             return Ok(_context.Book.ToList());
         }
+        [HttpPost("Search")]
+        public IActionResult Search(string s)
+        {
+            return Ok(_context.Book.Where(item => item.Title.Contains(s) || item.Author.Contains(s)).ToList());
+        }
+        [HttpPut("Add")]
+        public IActionResult Add(string title, string author)
+        {
+            Book book = new Book();
+            book.Title = title;
+            book.Author = author;
+            _context.Book.Add(book);
+            _context.SaveChanges();
+            return Ok(book);
+        }
+
+        [HttpPost("Update")]
+        public IActionResult Update(int id, string title, string author)
+        {
+            Book book = new Book();
+            book.Id = id;
+            book.Title = title;
+            book.Author = author;
+            _context.Book.Update(book);
+            _context.SaveChanges();
+            return Ok(book);
+        }
+
+        [HttpPost("Delete")]
+        public IActionResult Delete(int id)
+        {
+            Book book = new Book();
+            book.Id = id;
+            _context.Book.Remove(book);
+            _context.SaveChanges();
+            return Ok(book);
+        }
     }
+}
 }
